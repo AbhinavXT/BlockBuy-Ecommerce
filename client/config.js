5f96916ce08f52cd03243c59a5331b98e583fd8c
AbhinavXT <abhinavpathaka17@gmail.com>
2022-09-19 11:28:58 +0530

    pitch

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-export const itemMarketAddress = '0x5321b1f4A4fa3F7a962E52E5b3012385A4c5C2e5'
-export const itemContractAddress = '0x4030A987d2A8AC149fcf25c4b6FcAc23e80b2Fdb'
+export const itemMarketAddress = '0x46CA9E3ca50C0cCfe9893B359E55ae03a9F9c311'
+export const itemContractAddress = '0x31de885D839d928c1Db82e649E0Bf2C1a5ce0C92'
